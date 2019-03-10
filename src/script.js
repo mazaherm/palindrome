d03ae842997efd75885778c482c300e060b8e6ce
Mazaher Muraj <mmuraj.313@gmail.com>
2019-03-10 19:59:46 +0000

    reverses display value and changes colour if palindrome or not

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
-function getDisplay()
-{
+function getDisplay() {
     let input = document.getElementById('input-text')
     let div = document.getElementById('output-display')
-    div.innerHTML = input.value
+
+    div.innerHTML = input.value.split('').reverse().join('')
+
+    if (div.innerHTML === input.value) {
+        document.getElementById("output-display").style.color = 'green'
+    } else {
+        document.getElementById("output-display").style.color = 'red'
+    }
 }
