bdb1e86d7f5db3beba4da0939fe189f9e5997dc2
Mazaher Muraj <mmuraj.313@gmail.com>
2019-03-17 12:43:49 +0000

    removes spacing in palindromes

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@ function getDisplay() {
     let input = document.getElementById('input-text')
     let div = document.getElementById('output-display')
 
-    div.innerHTML = input.value.split('').reverse().join('')
+    div.innerHTML = input.value.split('').reverse().join('').replace(/[\W_]+/g,"")
 
-    if (div.innerHTML.toLowerCase() === input.value.toLowerCase()) {
+    if (div.innerHTML.toLowerCase().replace(/[\W_]+/g,"") === input.value.toLowerCase().replace(/[\W_]+/g,"")) {
         document.getElementById("output-display").style.color = 'green'
     } else {
         document.getElementById("output-display").style.color = 'red'
